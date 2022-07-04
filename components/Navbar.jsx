db42d454c858a19e4d1acf024dfdcb6ddf40e9b3
ufukcankurt <ufuk.cnk@gmail.com>
2022-07-04 14:24:10 +0300

    Create initial app

---
+++ components/Navbar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../styles/Navbar.module.css";
+
+const Navbar = () => {
+  return <div className={styles.container}>Navbar</div>;
+};
+
+export default Navbar;
---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Pizza Restaurant in NY</title>
+        <meta name="description" content="Best pizza shop in town" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      Homepage
+    </div>
+  )
+}
