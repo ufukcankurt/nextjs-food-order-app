ce7e41c208cd0674c432bfaf8f312ae9283269d2
ufukcankurt <ufuk.cnk@gmail.com>
2022-07-06 23:37:50 +0300

    Update | logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
           <li className={styles.listItem}>Homepage</li>
           <li className={styles.listItem}>Products</li>
           <li className={styles.listItem}>Menu</li>
-          <Image src="/img/logo.png" alt="" width="160px" height="69px" />
+          <Image src="/img/logo.png" alt="" width="75px" height="75px" />
           <li className={styles.listItem}>Events</li>
           <li className={styles.listItem}>Blog</li>
           <li className={styles.listItem}>Contact</li>
