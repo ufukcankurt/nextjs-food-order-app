5cd001e22e2a43ecc858e0c40aa49007bfbe2d62
ufukcankurt <ufuk.cnk@gmail.com>
2022-07-08 17:56:25 +0300

    Create | Login Page for Admin

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -121,7 +121,18 @@ const Index = ({ orders, products }) => {
   );
 };
 
-export const getServerSideProps = async () => {
+export const getServerSideProps = async (ctx) => {
+  const myCookie = ctx.req?.cookies || "";
+
+  if (myCookie.token !== process.env.TOKEN) {
+    return {
+      redirect: {
+        destination: "/admin/login",
+        permanent: false,
+      },
+    };
+  }
+
   const productRes = await axios.get("http://localhost:3000/api/products");
   const orderRes = await axios.get("http://localhost:3000/api/orders");
 
