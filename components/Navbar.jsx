dba08f89d9a59a7080bfe35c46e805f97737bfc2
ufukcankurt <ufuk.cnk@gmail.com>
2022-07-08 13:28:45 +0300

    Create & Update | Link at Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -19,7 +19,9 @@ const Navbar = () => {
       </div>
       <div className={styles.item}>
         <ul className={styles.list}>
-          <li className={styles.listItem}>Homepage</li>
+          <Link href="/" passHref>
+            <li className={styles.listItem}>Homepage</li>
+          </Link>
           <li className={styles.listItem}>Products</li>
           <li className={styles.listItem}>Menu</li>
           <Image src="/img/logo.png" alt="" width="75px" height="75px" />
