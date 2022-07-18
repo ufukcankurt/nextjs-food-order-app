06538fcb0611d16a6963396ea9d5ccc836861469
ufukcankurt <ufuk.cnk@gmail.com>
2022-07-18 07:30:16 +0300

    Update & Fix | bugs

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,56 +1,49 @@
-import styles from "../styles/Footer.module.css"
-import React from 'react'
-import Image from "next/image"
+import styles from "../styles/Footer.module.css";
+import React from "react";
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <div className={styles.container}>
-    <div className={styles.item}>
-      <Image src="/img/bg.png" layout="fill" alt="" />
-    </div>
-    <div className={styles.item}>
-      <div className={styles.card}>
-        <h2 className={styles.motto}>
-          OH YES, WE DID. THE UCK PIZZA, WELL BAKED SLICE OF PIZZA
-        </h2>
+      <div className={styles.item}>
+        <Image src="/img/bg.png" layout="fill" alt="" />
       </div>
-      <div className={styles.card}>
-        <h1 className={styles.title}>FIND OUR RESTAURANTS</h1>
-        <p className={styles.text}>
-          1654 R. Don Road #304. <br/>
-          Washington, DC <br/>
-          {`(602) 867-2233`}
-        </p>
-        <p className={styles.text}>
-          1654 R. Don Road #304. <br/>
-          Washington, DC <br/>
-          {`(602) 867-2233`}
-        </p>
-        <p className={styles.text}>
-          1654 R. Don Road #304. <br/>
-          Washington, DC <br/>
-          {`(602) 867-2233`}
-        </p>
-        <p className={styles.text}>
-          1654 R. Don Road #304. <br/>
-          Washington, DC <br/>
-          {`(602) 867-2233`}
-        </p>
-        <h1 className={styles.title}>WORKING HOURS</h1>
-        <p className={styles.text}>
-          MONDAY UNTIL FRIDAY <br/>
-          9:00 - 22:00
-        </p>
-        <p className={styles.text}>
-          SATURDAY - SUNDAY <br/>
-          12:00 - 24:00
-        </p>
-      </div>
-      <div className={styles.card}></div>
-    </div>
-      
-    </div>
-  )
-}
+      <div className={styles.item}>
+        <div className={styles.card}>
+          <h2 className={styles.motto}>
+            OH YES, WE DID. THE UCK PIZZA, WELL BAKED SLICE OF PIZZA
+          </h2>
+        </div>
+        <div className={`${styles.card} ${styles.flex}`}>
+          <div className={styles.restaurants}>
+            <h1 className={styles.title}>FIND OUR RESTAURANTS</h1>
 
-export default Footer
+            <p className={styles.text}>
+              1654 R. Don Road #304. <br />
+              Washington, DC <br />
+              {`(602) 867-2233`}
+            </p>
+            <p className={styles.text}>
+              1654 R. Don Road #304. <br />
+              Washington, DC <br />
+              {`(602) 867-2233`}
+            </p>
+          </div>
+          <div className={styles.hours}>
+            <h1 className={styles.title}>WORKING HOURS</h1>
+            <p className={styles.text}>
+              MONDAY UNTIL FRIDAY <br />
+              9:00 - 22:00
+            </p>
+            <p className={styles.text}>
+              SATURDAY - SUNDAY <br />
+              12:00 - 24:00
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
